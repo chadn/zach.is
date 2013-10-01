8691979027a99fae088aef942d7989a1b0a43bd4
Zach Heineman <zach@heineman.info>
2013-10-01 14:35:33 -0700

    Move data out of main.js

--- js/main.js
+++ js/main.js
@@ -8,36 +8,6 @@ $.ui.autocomplete.filter = function (array, term) {
 
 $(function() {
 
-  // create the autocomplete source array
-  var alpha = [
-    'alpha',
-    'bravo',
-    'charlie',
-    'delta',
-    'echo',
-    'foxtrot',
-    'golf',
-    'hotel',
-    'india',
-    'juliett',
-    'kilo',
-    'lima',
-    'mike',
-    'november',
-    'oscar',
-    'papa',
-    'quebec',
-    'romeo',
-    'sierra',
-    'tango',
-    'uniform',
-    'victor',
-    'whiskey',
-    'xray',
-    'yankee',
-    'zulu'
-  ];
-
   // initialize the autocomplete
   $('#1').autocomplete({
     minLength: 0,
