f5c158345fcc3cce09a8666b259ae832de6062bd
Zach Heineman <zach@heineman.info>
2013-10-01 14:41:40 -0700

    Break out autocomplete setup into a function

--- js/main.js
+++ js/main.js
@@ -8,30 +8,38 @@ $.ui.autocomplete.filter = function (array, term) {
 
 $(function() {
 
-  // initialize the autocomplete
-  $('#1').autocomplete({
-    minLength: 0,
-    source: alpha,
-    change: function(e, ui) { console.log('Changed! (autocomplete)') },
-    close: function(e, ui) { console.log('Closed! (autocomplete)') },
-    create: function(e, ui) { 
-      console.log('Created! (autocomplete)');
-      $('#1').attr('placeholder', alpha[0]);
-    },
-    focus: function(e, ui) {
-      console.log('Focused! (autocomplete)');
-      // return false; // this will prevent the placeholder from being populated with the focused item
-    },
-    open: function(e, ui ) { console.log('Opened! (autocomplete)') },
-    response: function(e, ui) { console.log('Responsed! (autocomplete)') },
-    search: function(e, ui) { console.log('Searched! (autocomplete)') },
-    select: function(e, ui) {
-      console.log('Selected! (autocomplete)');
-    }
-  });
+  var setupAutocomplete = function(id) {
 
-  // automatically open the autocomplete
-  $('#1').autocomplete('search', '');
+    var $el = $('#' + id); 
+
+    // initialize the autocomplete
+    $el.autocomplete({
+      minLength: 0,
+      source: alpha,
+      change: function(e, ui) { console.log('Changed! (autocomplete)') },
+      close: function(e, ui) { console.log('Closed! (autocomplete)') },
+      create: function(e, ui) { 
+        console.log('Created! (autocomplete)');
+        $('#1').attr('placeholder', alpha[0]);
+      },
+      focus: function(e, ui) {
+        console.log('Focused! (autocomplete)');
+        // return false; // this will prevent the placeholder from being populated with the focused item
+      },
+      open: function(e, ui ) { console.log('Opened! (autocomplete)') },
+      response: function(e, ui) { console.log('Responsed! (autocomplete)') },
+      search: function(e, ui) { console.log('Searched! (autocomplete)') },
+      select: function(e, ui) {
+        console.log('Selected! (autocomplete)');
+      }
+    });
+
+    // automatically open the autocomplete
+    $el.autocomplete('search', '');
+
+  }
+
+  setupAutocomplete(1);
 
   $('#1').on('focus', function(){
     $('#1').val('');
